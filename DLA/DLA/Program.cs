bd8c47577b5ca4d9d0ec33100b5f695e44db2ba0
Rainbowsole2021 <122910261+Rainbowsole2021@users.noreply.github.com>
2025-01-10 10:41:10 +0100

    Version 1.1

--- DLA/DLA/Program.cs
+++ DLA/DLA/Program.cs
@@ -9,8 +9,6 @@ namespace DLA
         public static int height = 300;
         public static int width = 300;
         public static int loops = 10_000;
-        public static string paintPath = "YOUR_MS_PAINT_PATH";
-        public static string path = "YOUR_DLA_IMAGE_PATH";
 
         private const int SW_HIDE = 0;
         private const int SW_SHOW = 5;
@@ -19,6 +17,60 @@ namespace DLA
         {
             Console.WriteLine("Working...");
 
+            string paintPath = GetPaintPath();
+            string path = GetSavePath();
+            bool[,] cords = DLA();
+
+            using Bitmap map = new Bitmap(width, height);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (cords[x, y])
+                        map.SetPixel(x, y, Color.Black);
+                }
+            }
+            map.Save(path);
+
+            using Process process = Process.Start(paintPath, path);
+            IntPtr console = GetConsoleWindow();
+            ShowWindow(console, SW_HIDE);
+
+            process.WaitForExit();
+        }
+
+        private static string GetSavePath()
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            return desktop + @"\DLAbitmap.bmp";
+        }
+
+        private static string GetPaintPath()
+        {
+            string[] userFiles = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\Local\Microsoft\WindowsApps\", "*.exe", new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            });
+
+            foreach(string file in userFiles)
+            {
+                if (Path.GetFileNameWithoutExtension(file) == "mspaint")
+                    return file;
+            }
+
+            return "";
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static bool[,] DLA()
+        {
             bool[,] cords = new bool[height, width];
 
             Random random = new Random();
@@ -43,54 +95,37 @@ namespace DLA
                 {
                     do
                     {
-                        int xDir = random.Next(-1, 2);
-                        int yDir = random.Next(-1, 2);
-
-                        xCord = Math.Clamp(xCord + xDir, 0, width - 1);
-                        yCord = Math.Clamp(yCord + yDir, 0, height - 1);
+                        int dir = random.Next(0, 4);
+                        switch (dir)
+                        {
+                            case 0:
+                                xCord = Math.Clamp(xCord + 1, 0, width - 1);
+                                break;
+                            case 1:
+                                xCord = Math.Clamp(xCord - 1, 0, width - 1);
+                                break;
+                            case 2:
+                                yCord = Math.Clamp(yCord + 1, 0, height - 1);
+                                break;
+                            case 3:
+                                yCord = Math.Clamp(yCord - 1, 0, height - 1);
+                                break;
+                        }
                     } while (!HasNeighbours(xCord, yCord, cords));
 
                     cords[xCord, yCord] = true;
                 }
             }
 
-            using Bitmap map = new Bitmap(width, height);
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (cords[x, y])
-                        map.SetPixel(x, y, Color.Black);
-                }
-            }
-
-            map.SetPixel(startX, startY, Color.Red);
-            map.Save(path);
-
-            using Process process = Process.Start(paintPath, path);
-            IntPtr console = GetConsoleWindow();
-            ShowWindow(console, SW_HIDE);
-
-            process.WaitForExit();
+            return cords;
         }
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         public static bool HasNeighbours(int x, int y, bool[,] cords)
         {
             return cords[Math.Clamp(x + 1, 0, width - 1), y] ||
                    cords[Math.Clamp(x - 1, 0, width - 1), y] ||
                    cords[x, Math.Clamp(y + 1, 0, height - 1)] ||
-                   cords[x, Math.Clamp(y - 1, 0, height - 1)] ||
-                   cords[Math.Clamp(x + 1, 0, width - 1), Math.Clamp(y + 1, 0, height - 1)] ||
-                   cords[Math.Clamp(x - 1, 0, width - 1), Math.Clamp(y - 1, 0, height - 1)] ||
-                   cords[Math.Clamp(x + 1, 0, width - 1), Math.Clamp(y - 1, 0, height - 1)] ||
-                   cords[Math.Clamp(x - 1, 0, width - 1), Math.Clamp(y + 1, 0, height - 1)];
+                   cords[x, Math.Clamp(y - 1, 0, height - 1)];
         }
 
         public static bool ArrayFull(bool[,] cords)
